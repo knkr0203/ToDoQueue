9967a8c6ecf8d97f814152763f411b9a212595a5
knkr0203 <masked_universe@yahoo.co.jp>
2019-07-22 11:08:33 +0900

    first commit

---
+++ src/main/java/com/kenkoro/springboot/ToDoQueueApplication.java
@@ -0,0 +1,13 @@
+package com.kenkoro.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoQueueApplication.class, args);
+	}
+
+}
